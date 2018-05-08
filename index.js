4057dc532b5f52ae10372ad24ab4e34209712003
Hankil Chun <hankil.chun@gmail.com>
2018-05-08 19:53:19 +0900

    begin

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var cors = require('cors');
 var path = require('path');
-var app = require(__dirname + '/app.js');
-//var app = express();
+var bodyParser = require('body-parser');
+
+var app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, '/client/public')));
-console.log(__dirname);
+
 app.get('/test', function (req, res) {
     res.send('Hello World');
 })
 
 app.listen(3001);
-console.log('listening');
+console.log('listening, 3001');

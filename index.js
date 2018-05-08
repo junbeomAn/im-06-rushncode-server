294388a0709a922cf7ef8e2d02deec32b0e1f2fe
Hankil Chun <hankil.chun@gmail.com>
2018-05-08 19:44:27 +0900

    test

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express = require('express');
-var app = express();
+var cors = require('cors');
+var path = require('path');
+var app = require(__dirname + '/app.js');
+//var app = express();
 
-
-app.get('/', function (req, res) {
+app.use(cors());
+app.use('/', express.static(path.join(__dirname, '/client/public')));
+console.log(__dirname);
+app.get('/test', function (req, res) {
     res.send('Hello World');
 })
 
-app.listen(3000);
+app.listen(3001);
 console.log('listening');

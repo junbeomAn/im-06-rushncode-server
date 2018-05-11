75ddf4e3b87db1a889c85b2a190e242888e07549
Junbeom_An <wnsqja8744@gmail.com>
2018-05-11 14:25:02 +0900

    signIn changed

--- controller/auth/signIn.js
+++ controller/auth/signIn.js
@@ -1,38 +1,36 @@
-const bcrypt = require('bcrypt-nodejs');
-const model = require('../../model/signInSignUp');
+const bcrypt = require("bcrypt-nodejs");
+const model = require("../../model/signInSignUp");
 
 const signin = (req, res) => {
-    const {
-        email,
-        password
-    } = req.body;
-    console.log(req.body);
-    model.checkUsername(email, (err, result) => {
-        if (err) {
-            res.send(err);
-        } else {
-            if (result.length !== 0) {
-                bcrypt.compare(password, result[0].password, function (err, truth) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        if (truth) {
-                            console.log('login success');
-                            res.send('success');
-                            return;
-                        } else {
-                            console.log('login fail');
-                            res.send(false);
-                            return;
-                        }
-                    }
-                });
+  const { email, password } = req.body;
+  console.log(req.body);
+  model.checkEmail(email, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      if (result.length !== 0) {
+        bcrypt.compare(password, result[0].password, function(err, truth) {
+          if (err) {
+            console.log(err);
+          } else {
+            if (truth) {
+              console.log("login success");
+              res.redirect("http://localhost:3000/");
+              //   res.send('success');
+              return;
             } else {
-                console.log('not exist');
-                res.send('not exist');
+              console.log("login fail");
+              res.send(false);
+              return;
             }
-        }
-    })
-}
+          }
+        });
+      } else {
+        console.log("not exist");
+        res.send("not exist");
+      }
+    }
+  });
+};
 
 module.exports = signin;
